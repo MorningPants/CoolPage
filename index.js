e2aac1018fda73dab150e2034346ed8c56a6ed85
MorningPants <77476821+MorningPants@users.noreply.github.com>
2021-11-22 13:05:30 -0800

    fix mobile scroll?

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let page = document.getElementById("page");
 let content = document.getElementsByClassName("content");
 
-
 page.style.position = "fixed";
 page.style.width = "100vw";
 page.style.transition = "top 1s ease-in-out";
@@ -10,20 +9,20 @@ page.style.transition = "top 1s ease-in-out";
 
 let view = 1;
 let transitioning = false;
-let maxview=3;
+let maxview = 3;
 
 function turnPage(event) {
   event.preventDefault();
   if (!transitioning) {
-      if (event.deltaY > 0 && view < maxview) {
-          view += 1;
-          transitioning = true;
-        }
-        if (event.deltaY < 0 && view > 1) {
-            view -= 1;
-            transitioning = true;
-        }
-        page.className = `page${view}`;
+    if (event.deltaY > 0 && view < maxview) {
+      view += 1;
+      transitioning = true;
+    }
+    if (event.deltaY < 0 && view > 1) {
+      view -= 1;
+      transitioning = true;
+    }
+    page.className = `page${view}`;
   }
 }
 page.onwheel = turnPage;
@@ -32,29 +31,34 @@ page.onwheel = turnPage;
 let touchY = 0;
 var src = document.getElementById("page");
 
-src.addEventListener('touchstart', function(e) {
-    touchY = e.touches[0].clientY
-    console.log(touchY)
-},false)
+src.addEventListener(
+  "touchstart",
+  function (e) {
+    touchY = e.touches[0].clientY;
+    console.log(touchY);
+  },
+  false
+);
 
 page.ontouchmove = (event) => {
-    if (event.touches[0].clientY < touchY && view < maxview){
-        view += 1;
-        transitioning = true;
+  if (!transitioning) {
+    if (event.touches[0].clientY < touchY && view < maxview) {
+      view += 1;
+      transitioning = true;
     }
     if (event.touches[0].clientY > touchY && view > 1) {
-        view -= 1;
-        transitioning = true;
+      view -= 1;
+      transitioning = true;
     }
     page.className = `page${view}`;
-
+  }
 };
-refresh = function(){
-    transitioning = false;
-    console.log('time')
-}
+refresh = function () {
+  transitioning = false;
+  console.log("time");
+};
 
 page.ontransitionend = () => {
-    setTimeout(refresh, 1000)
-    console.log(view)
-}
+  setTimeout(refresh, 500);
+  console.log(view);
+};
